6503902110bce1fe602ee4c3f8e2a9d46b9e3680
Giada15 <tascheriogiada@gmail.com>
2024-04-15 14:44:34 -0400

    Implemented function that returns a String value based on a switch condition

--- ReturnValue.java
+++ ReturnValue.java
@@ -4,10 +4,37 @@ public class ReturnValue {
            double area1 = calculateArea(2.3, 3.6);
            double area2= calculateArea(1.5, 6.3);
            double area3= calculateArea(8.7, 1.2);
+           calculateArea(-5, 2);
+
+           String englishExplanation = explainArea("English");
+           String frenchExplanation = explainArea("French");
+           String spanishExplanation = explainArea("Spanish");
+           String italianExplanation = explainArea("Italian");
         }
     
         public static double calculateArea(double length, double width){
+            if(length<0 || width<0){
+                System.out.println("INVALID DIMENSIONS");
+                System.exit(0);
+            }
             double area = length*width;
             return area;
         }
+
+        public static String explainArea(String language){
+            switch(language){
+                case "English": return "Area equals length * width";
+                case "French": return "La surface est egale a la longueur * la largeur"; 
+                case "Spanish": return "area es igual a largo * ancho"; 
+                default: return "Language not available"; 
+            }
+
+        }
 }
+
+
+// English "Area equals length * width"
+
+// French "La surface est egale a la longueur * la largeur"
+
+// Spanish "area es igual a largo * ancho"
