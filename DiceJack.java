67f67bcca44acacf63ecebab9320c871309d5904
Giada15 <tascheriogiada@gmail.com>
2024-04-16 12:52:23 -0400

    Dice project: Implemented logic to handle invalid numbers and determine the winner based on game rules.

--- DiceJack.java
+++ DiceJack.java
@@ -7,27 +7,52 @@ public class DiceJack {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Pick three numbers between 1 and 6");
-        int userNumber1 = scan.nextInt();
-        int userNumber2 = scan.nextInt();
-        int userNumber3 = scan.nextInt();
+        System.out.print("Enter three numbers between 1 and 6: ");
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+        int num3 = scan.nextInt();
 
         int roll1= rollDice();
         int roll2= rollDice();
         int roll3= rollDice();
 
-        int userSum = userNumber1+userNumber2+userNumber3;
-        int rollSum = roll1+roll2+roll3;
+        if(isLessThan1(num1, num2, num3) || isHigherThan6(num1, num2, num3)){
+            System.out.println("You entered numbers outside the valid range");
+            System.exit(0);
+        }
 
-        System.out.println("User numbers: " + userNumber1 + " " + userNumber2 + " "+ userNumber3 + " " );
-        System.out.println("Rolled numbers: " + roll1 + " " + roll2 + " "+ roll3 + " " );   
+        int sumOfNumbers = num1+num2+num3;
+        int diceSum = roll1+roll2+roll3;
+
+        System.out.println("Your Sum: " + sumOfNumbers + "  Computer Sum: " + diceSum);
+       
+
+        if(userWon(sumOfNumbers, diceSum)){
+            System.out.println("\nCongratulations! You won!");
+        }else{
+            System.out.println("\nBetter luck next time");
+        }
+
+        scan.close();
         
+    }
+
+    public static boolean isLessThan1(int num1, int num2, int num3){
+        return (num1 < 1 || num2 < 1 || num3 < 1);
+    }
+
+    public static boolean isHigherThan6(int num1, int num2, int num3){
+        return (num1 > 6 || num2 > 6 || num3 > 6);
     }
 
     public static int rollDice(){
         double randomNumber = (Math.random() * 6) + 1;
         return (int)randomNumber;
 
+    }
+
+    public static boolean userWon(int sumNumbers, int sumDiceRolls){
+        return (sumNumbers>sumDiceRolls && (sumNumbers - sumDiceRolls) <5);
     }
 
    
